183df77c1d554cb5f453c92bd141219045181989
Clemens Anzmann <clemens.anzmann@googlemail.com>
2018-11-11 13:28:01 +0000

    add npm update functions

--- update-packages.sh
+++ update-packages.sh
@@ -7,6 +7,7 @@ echo "
 ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝     ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
 ----------------------------------------------------------------------------------------------------------------------
 "
+date
 
 echo "Updating Homebrew packages..."
 brew ls
@@ -21,3 +22,11 @@ brew doctor
 echo "Updating Get settings..."
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
 curl -O https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt ~/gitalias.txt
+
+echo "Updating npm & packages..."
+npm install -g npm@latest
+npm outdated -g
+npm update -g
+
+echo "Done."
+date
