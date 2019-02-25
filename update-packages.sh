8fc5700ff95a28d86f834540e060d89af70566c8
Clemens Anzmann <clemens.anzmann@gmail.com>
2019-02-25 11:56:16 +0000

    fix bash script issues

--- update-packages.sh
+++ update-packages.sh
@@ -15,13 +15,12 @@ echo ""
 
 brew update
 brew upgrade
-brew prune
-brew cleanup
+brew cleanup --prune-prefix
 brew doctor
 
 echo "Updating Get settings..."
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-curl -O https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt ~/gitalias.txt
+curl https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt -o ~/gitalias.txt
 
 echo "Updating npm & packages..."
 npm install -g npm@latest
