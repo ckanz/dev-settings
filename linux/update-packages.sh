99b21bfdc7fd3d7be7b1f629ceae6c57c1614e64
Clemens Anzmann <clemens.anzmann@googlemail.com>
2026-04-29 09:49:00 +1000

    trim old settings

--- linux/update-packages.sh
+++ linux/update-packages.sh
@@ -7,29 +7,15 @@ echo "
 ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝     ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
 ----------------------------------------------------------------------------------------------------------------------
 "
-date
 
 echo "Updating packages..."
 sudo apt --assume-yes  update
 sudo apt --assume-yes upgrade
+sudo snap refresh
 
-echo "Updating Git settings..."
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-curl https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt -o ~/gitalias.txt
-
-echo "Updating npm & packages..."
-sudo npm install -g npm@latest
-sudo npm outdated -g
-sudo npm update -g
-
-git fetch
-git pull
-
-echo "Updating bash, vim and tmux settings"
-cp .vimrc ~/.vimrc
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-cp .tmux.conf ~/.tmux.conf
-cp .bash_profile ~/.bash_profile
+# echo "Updating npm & packages..."
+# sudo npm install -g npm@latest
+# sudo npm outdated -g
+# sudo npm update -g
 
 echo "Done."
-date
